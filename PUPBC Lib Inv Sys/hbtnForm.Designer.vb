b6e809de41945df8a5f58fc8a3a43b115ba0fb64
YuehanKaichi <dichosojohncarlo@gmail.com>
2023-06-13 18:52:18 +0800

    Add Home Button

---
+++ PUPBC Lib Inv Sys/hbtnForm.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class hbtnForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dt1 = New System.Windows.Forms.DataGridView()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.dt1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dt1
+        '
+        Me.dt1.AllowUserToAddRows = False
+        Me.dt1.AllowUserToDeleteRows = False
+        Me.dt1.AllowUserToOrderColumns = True
+        Me.dt1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dt1.Location = New System.Drawing.Point(0, 104)
+        Me.dt1.Name = "dt1"
+        Me.dt1.ReadOnly = True
+        Me.dt1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
+        Me.dt1.Size = New System.Drawing.Size(905, 424)
+        Me.dt1.TabIndex = 1
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.LightGreen
+        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(817, 39)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 34)
+        Me.Button6.TabIndex = 2
+        Me.Button6.Text = "Add Book"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(817, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'hbtnForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(904, 528)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.dt1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "hbtnForm"
+        Me.Text = "hbtnForm"
+        CType(Me.dt1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dt1 As DataGridView
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ PUPBC Lib Inv Sys/hbtnForm.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+Public Class hbtnForm
+    Dim conn As New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True")
+    Private Sub dt1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dt1.CellContentClick
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        addForm.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim table As New DataTable()
+        Dim adapter As New SqlDataAdapter("SELECT * FROM BOOKS", conn)
+        adapter.Fill(table)
+
+        dt1.DataSource = table
+    End Sub
+End Class
