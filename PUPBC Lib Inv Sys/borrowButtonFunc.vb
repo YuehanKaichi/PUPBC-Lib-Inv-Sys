d218fa891c4a149f8c689172f645c224e5e3eae2
YuehanKaichi <dichosojohncarlo@gmail.com>
2023-07-11 16:22:41 +0800

    Update codes to update availability of books

--- PUPBC Lib Inv Sys/borrowButtonFunc.vb
+++ PUPBC Lib Inv Sys/borrowButtonFunc.vb
@@ -1,63 +1,75 @@
 ﻿Imports System.Data.SqlClient
 Public Class borrowButtonFunc
     Dim connectionString As String = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True"
-    Dim conn As New SqlConnection(connectionString)
+    Dim connection As New SqlConnection(connectionString)
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Close()
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim bookID As Integer = Integer.Parse(bID.Text)
-        Dim studentID As String = sID.Text
+        Dim studentID As String = Convert.ToInt32(sID.Text)
         Dim studentName As String = sName.Text
-        Dim cnYear As String = cnYeartxt.Text
+        Dim courseAndYear As String = cnYeartxt.Text
+        Dim bookID As String = bID.Text
         Dim borrowDate As DateTime = DateTime.Now
         Dim dueDate As DateTime = borrowDate.AddDays(3)
 
-        ' Check if the book exists in the book database
-        If IsBookAvailable(connectionString, bookID) Then
-            ' Book is available, proceed with borrowing
+        Dim bookExists As Boolean = False
+        Dim bookAvailable As Boolean = False
 
-            ' Perform the insert operation into the borrow table
-            InsertBorrowRecord(connectionString, studentID, studentName, cnYear, bookID, borrowDate, dueDate)
+        Using connection As New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True")
+            connection.Open()
 
-            ' Display a success message
-            MessageBox.Show("Book has been borrowed", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Dim checkBookQuery As String = "SELECT * FROM pupLibBooks WHERE book_ID = @bookID AND avail = 'available'"
+            Using command As New SqlCommand(checkBookQuery, connection)
+                command.Parameters.AddWithValue("@bookID", bookID)
+                Dim reader As SqlDataReader = command.ExecuteReader()
+                If reader.HasRows Then
+                    bookExists = True
+                    bookAvailable = True
+                End If
+            End Using
+
+            connection.Close()
+        End Using
+
+        If bookExists AndAlso bookAvailable Then
+            Using connection As New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True")
+                connection.Open()
+
+                Dim insertQuery As String = "INSERT INTO pupLibBorrow (studentID, studentName, cnYear, book_ID, borrowDate, dueDate) VALUES (@studentID, @studentName, @courseAndYear, @bookID, @borrowDate, @dueDate)"
+                Using command As New SqlCommand(insertQuery, connection)
+                    command.Parameters.AddWithValue("@studentID", studentID)
+                    command.Parameters.AddWithValue("@studentName", studentName)
+                    command.Parameters.AddWithValue("@courseAndYear", courseAndYear)
+                    command.Parameters.AddWithValue("@bookID", bookID)
+                    command.Parameters.AddWithValue("@borrowDate", borrowDate)
+                    command.Parameters.AddWithValue("@dueDate", dueDate)
+                    command.ExecuteNonQuery()
+                End Using
+
+                connection.Close()
+            End Using
+
+            ' Update the pupLibBooks table to mark the book as borrowed
+            Using connection As New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True")
+                connection.Open()
+
+                Dim updateQuery As String = "UPDATE pupLibBooks SET avail = 'borrowed' WHERE book_ID = @bookID"
+                Using command As New SqlCommand(updateQuery, connection)
+                    command.Parameters.AddWithValue("@bookID", bookID)
+                    command.ExecuteNonQuery()
+                End Using
+
+                connection.Close()
+            End Using
+
+            MessageBox.Show("Book borrowed successfully.", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information)
         Else
-            ' Book is not available
-            MessageBox.Show("Book not available.")
+            MessageBox.Show("The book is not available.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
+
+
     End Sub
 
-    Private Function IsBookAvailable(connectionString As String, bookID As Integer) As Boolean
-        Dim query As String = "SELECT COUNT(*) FROM pupLibBooks WHERE Book_ID = @BookID"
-        Dim isAvailable As Boolean = False
 
-        Using connection As New SqlConnection(connectionString)
-            Using command As New SqlCommand(query, connection)
-                command.Parameters.AddWithValue("@BookID", bookID)
-                connection.Open()
-                Dim count As Integer = CInt(command.ExecuteScalar())
-                isAvailable = count > 0
-            End Using
-        End Using
-
-        Return isAvailable
-    End Function
-
-    Private Sub InsertBorrowRecord(connectionString As String, studentID As String, studentName As String, cnYear As String, bookID As Integer, borrowDate As DateTime, dueDate As DateTime)
-        Dim query As String = "INSERT INTO pupLibBorrow (studentID, studentName, cnYear, Book_ID, BorrowDate, DueDate) VALUES (@StudentID, @StudentName, @CNYear, @BookID, @BorrowDate, @DueDate)"
-
-        Using connection As New SqlConnection(connectionString)
-            Using command As New SqlCommand(query, connection)
-                command.Parameters.AddWithValue("@StudentID", studentID)
-                command.Parameters.AddWithValue("@StudentName", studentName)
-                command.Parameters.AddWithValue("@CNYear", cnYear)
-                command.Parameters.AddWithValue("@BookID", bookID)
-                command.Parameters.AddWithValue("@BorrowDate", SqlDbType.Date).Value = borrowDate
-                command.Parameters.AddWithValue("@DueDate", SqlDbType.Date).Value = dueDate
-                connection.Open()
-                command.ExecuteNonQuery()
-            End Using
-        End Using
-    End Sub
 End Class
