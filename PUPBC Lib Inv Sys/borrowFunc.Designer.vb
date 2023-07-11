c2cec48b9585f6cf47f9a6cd994a8ad943b4db56
YuehanKaichi <dichosojohncarlo@gmail.com>
2023-07-11 15:05:45 +0800

    Update Borrow Panel

--- PUPBC Lib Inv Sys/borrowFunc.Designer.vb
+++ PUPBC Lib Inv Sys/borrowFunc.Designer.vb
@@ -22,32 +22,37 @@ Partial Class Borrow
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dt1 = New System.Windows.Forms.DataGridView()
         Me.borBut = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.returnBut = New System.Windows.Forms.Button()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.statusBox = New System.Windows.Forms.ComboBox()
+        Me.refreshButton = New System.Windows.Forms.Button()
+        CType(Me.dt1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'dt1
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.AllowUserToOrderColumns = True
-        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.GridColor = System.Drawing.Color.White
-        Me.DataGridView1.Location = New System.Drawing.Point(-1, 136)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(906, 410)
-        Me.DataGridView1.TabIndex = 0
+        Me.dt1.AllowUserToAddRows = False
+        Me.dt1.AllowUserToDeleteRows = False
+        Me.dt1.AllowUserToOrderColumns = True
+        Me.dt1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dt1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dt1.BackgroundColor = System.Drawing.Color.White
+        Me.dt1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dt1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.dt1.Enabled = False
+        Me.dt1.GridColor = System.Drawing.Color.White
+        Me.dt1.Location = New System.Drawing.Point(0, 105)
+        Me.dt1.Name = "dt1"
+        Me.dt1.ReadOnly = True
+        Me.dt1.Size = New System.Drawing.Size(904, 439)
+        Me.dt1.TabIndex = 0
         '
         'borBut
         '
-        Me.borBut.Location = New System.Drawing.Point(745, 58)
+        Me.borBut.Location = New System.Drawing.Point(817, 39)
         Me.borBut.Name = "borBut"
         Me.borBut.Size = New System.Drawing.Size(75, 23)
         Me.borBut.TabIndex = 1
@@ -57,7 +62,7 @@ Partial Class Borrow
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(32, 74)
+        Me.Label1.Location = New System.Drawing.Point(35, 39)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(41, 13)
         Me.Label1.TabIndex = 2
@@ -65,65 +70,67 @@ Partial Class Borrow
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(79, 72)
+        Me.TextBox1.Location = New System.Drawing.Point(82, 37)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(210, 20)
         Me.TextBox1.TabIndex = 3
         '
         'returnBut
         '
-        Me.returnBut.Location = New System.Drawing.Point(745, 87)
+        Me.returnBut.Location = New System.Drawing.Point(736, 72)
         Me.returnBut.Name = "returnBut"
         Me.returnBut.Size = New System.Drawing.Size(75, 23)
         Me.returnBut.TabIndex = 4
         Me.returnBut.Text = "Return"
         Me.returnBut.UseVisualStyleBackColor = True
         '
-        'ComboBox1
+        'statusBox
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Before due", "Past due"})
-        Me.ComboBox1.Location = New System.Drawing.Point(79, 109)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox1.TabIndex = 5
+        Me.statusBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.statusBox.FormattingEnabled = True
+        Me.statusBox.Items.AddRange(New Object() {"Before due", "Past due", "All"})
+        Me.statusBox.Location = New System.Drawing.Point(106, 74)
+        Me.statusBox.Name = "statusBox"
+        Me.statusBox.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.statusBox.Size = New System.Drawing.Size(100, 21)
+        Me.statusBox.TabIndex = 5
         '
-        'Label2
+        'refreshButton
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(32, 112)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(37, 13)
-        Me.Label2.TabIndex = 6
-        Me.Label2.Text = "Status"
+        Me.refreshButton.Location = New System.Drawing.Point(817, 72)
+        Me.refreshButton.Name = "refreshButton"
+        Me.refreshButton.Size = New System.Drawing.Size(75, 23)
+        Me.refreshButton.TabIndex = 6
+        Me.refreshButton.Text = "Refresh"
+        Me.refreshButton.UseVisualStyleBackColor = True
         '
         'Borrow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(904, 544)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.refreshButton)
+        Me.Controls.Add(Me.statusBox)
         Me.Controls.Add(Me.returnBut)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.borBut)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dt1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Borrow"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form3"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dt1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents dt1 As DataGridView
     Friend WithEvents borBut As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents returnBut As Button
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents Label2 As Label
+    Friend WithEvents statusBox As ComboBox
+    Friend WithEvents refreshButton As Button
 End Class
--- PUPBC Lib Inv Sys/borrowFunc.vb
+++ PUPBC Lib Inv Sys/borrowFunc.vb
@@ -1,5 +1,74 @@
-﻿Public Class Borrow
+﻿Imports System.Data.SqlClient
+Public Class Borrow
+    Dim conn As New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=libinvsystem;Integrated Security=True")
     Private Sub borBut_Click(sender As Object, e As EventArgs) Handles borBut.Click
         borrowButtonFunc.Show()
+    End Sub
+
+    Private Sub refreshButton_Click(sender As Object, e As EventArgs) Handles statusBox.TextChanged
+        If statusBox.Text = "Before due" Then
+            conn.Open()
+            Dim table As New DataTable()
+            Dim adapter As New SqlDataAdapter("SELECT pupLibBorrow.studentID as 'Student ID', pupLibBorrow.studentName as 'Student Name',
+                pupLibBorrow.cnYear as 'Course and Year', pupLibBorrow.Book_ID as 'Book ID', 
+                pupLibBooks.Book_title as 'Book Title', 
+                pupLibBorrow.borrowDate as 'Date Borrowed', pupLibBorrow.DueDate as 'Date to be returned' 
+                FROM pupLibBorrow
+                JOIN pupLibBooks ON pupLibBorrow.Book_ID = pupLibBooks.Book_ID
+                WHERE pupLibBorrow.DueDate > GETDATE();", conn)
+            adapter.Fill(table)
+
+            dt1.DataSource = table
+            conn.Close()
+        ElseIf statusBox.Text = "Past due" Then
+            conn.Open()
+            Dim table As New DataTable()
+            Dim adapter As New SqlDataAdapter("SELECT pupLibBorrow.studentID as 'Student ID', pupLibBorrow.studentName as 'Student Name',
+                pupLibBorrow.cnYear as 'Course and Year', pupLibBorrow.Book_ID as 'Book ID', 
+                pupLibBooks.Book_title as 'Book Title', 
+                pupLibBorrow.borrowDate as 'Date Borrowed', pupLibBorrow.DueDate as 'Date to be returned' 
+                FROM pupLibBorrow
+                JOIN pupLibBooks ON pupLibBorrow.Book_ID = pupLibBooks.Book_ID
+                WHERE pupLibBorrow.DueDate < GETDATE();", conn)
+            adapter.Fill(table)
+
+            dt1.DataSource = table
+            conn.Close()
+        Else
+            conn.Open()
+            Dim table As New DataTable()
+            Dim adapter As New SqlDataAdapter("SELECT pupLibBorrow.studentID as 'Student ID', pupLibBorrow.studentName as 'Student Name',
+        pupLibBorrow.cnYear as 'Course and Year', pupLibBorrow.Book_ID as 'Book ID', 
+        pupLibBooks.Book_title as 'Book Title', 
+        pupLibBorrow.borrowDate as 'Date Borrowed', pupLibBorrow.DueDate as 'Date to be returned' 
+        FROM pupLibBorrow
+        JOIN pupLibBooks ON pupLibBorrow.Book_ID = pupLibBooks.Book_ID;", conn)
+            adapter.Fill(table)
+
+            dt1.DataSource = table
+            conn.Close()
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim searchText As String = TextBox1.Text.Trim().ToLower()
+        Try
+            For Each row As DataGridViewRow In dt1.Rows
+                If row.IsNewRow Then Continue For
+
+                Dim matchFound As Boolean = False
+
+                For Each cell As DataGridViewCell In row.Cells
+                    If cell.Value IsNot Nothing AndAlso cell.Value.ToString().ToLower().Contains(searchText) Then
+                        matchFound = True
+                        Exit For
+                    End If
+                Next
+                row.Visible = matchFound
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Invalid input: " & ex.Message)
+        End Try
+
     End Sub
 End Class
